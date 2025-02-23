7927231de1d576a07ca871d9cc9d8bfd5eca6a17
Nitish Singh <nitishsingh925@gmail.com>
2025-02-23 22:07:46 +0530

    show user

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ import { db } from "./config/db.js";
 import { usersTable } from "./drizzle/schema.js";
 
 const main = async () => {
-  const insert = await db.insert(usersTable).values({
-    name: "Nitish Singh",
-    age: 25,
-    email: "nitishsingh@gmail.com",
-  });
+  // Insert User
+  // const insert = await db.insert(usersTable).values({
+  //   name: "Nitish Singh",
+  //   age: 25,
+  //   email: "nitishsingh@gmail.com",
+  // });
+  // show user
+  const user = await db.select().from(usersTable);
+  console.log(user);
 };
 
 main().catch((error) => {
