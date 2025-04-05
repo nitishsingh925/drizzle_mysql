f8f13b698658d8d1122e4b0794d0c591e9516886
Nitish Singh <76509326+nitishsingh925@users.noreply.github.com>
2025-04-05 22:34:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import { db } from "./config/db.js";
 import { usersTable } from "./drizzle/schema.js";
 
+
+
 const main = async () => {
   // Insert User
   // const insert = await db.insert(usersTable).values({
