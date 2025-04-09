be199a8b6f504249c564c55a7ffe66a77821a470
Nitish Singh <nitishsingh925@gmail.com>
2025-04-09 12:00:00 +0000

    insert show

--- app.js
+++ app.js
@@ -3,35 +3,31 @@ import { usersTable } from "./drizzle/schema.js";
 
 const main = async () => {
   // Insert User
-  // const insert = await db.insert(usersTable).values({
-  //   name: "Nitish Singh",
-  //   age: 25,
-  //   email: "nitishsingh@gmail.com",
-  // });
+  const insert = await db.insert(usersTable).values({
+    name: "Nitish Singh",
+    age: 25,
+    email: "nitishsingh@gmail.com",
+  });
+  console.log(insert);
+
   // show user
   // const user = await db.select().from(usersTable);
   // console.log(user);
-
   // user find with email id
   // const userWithEmail = await db
   //   .select()
   //   .from(usersTable)
   //   .where({ email: "nitishsingh@gmail.com" });
-
   // user update with email id
-
   // const userUpdateWithEmail = await db
   //   .update(usersTable)
   //   .set({ name: "Nitish" })
   //   .where({ email: "nitish@gmail.com" });
-
   // delete user with email id
-
-  const userDeleteWithEmail = await db
-    .delete(usersTable)
-    .where({ email: "nitish@gmail.com" });
-
-  console.log(userDeleteWithEmail);
+  // const userDeleteWithEmail = await db
+  //   .delete(usersTable)
+  //   .where({ email: "nitish@gmail.com" });
+  // console.log(userDeleteWithEmail);
 };
 
 main().catch((error) => {
